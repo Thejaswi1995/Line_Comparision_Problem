39aad9a4b03129eff39211e64c1258df5bf40730
Thejaswi <thejaswirl187@gmail.com>
2022-04-28 22:50:02 +0530

    Added: Calculating Length of line

--- src/com/bridgelabz/linecomparision/LineComparisonProblem.java
+++ src/com/bridgelabz/linecomparision/LineComparisonProblem.java
@@ -1,7 +1,19 @@
 package com.bridgelabz.linecomparision;
 
+import java.util.Scanner;
+
 public class LineComparisonProblem {
+    static double length1;
     public static void main(String[] args) {
-        System.out.println("Welcome to the branch");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first co-ordinator:");
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        System.out.println("Enter the second co-ordinator:");
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+        length1 = Math.sqrt(Math.pow((x2-x1),2)+ Math.pow((y2-y1),2));
+        System.out.println("The length of first line is :" +length1);
+
     }
 }
